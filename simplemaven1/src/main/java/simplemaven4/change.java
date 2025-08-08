bba9b33238c78d54b22652592905327d88b8ecb7
DELL <DELL@DESKTOP-TBCA5D8>
2025-08-08 11:35:48 +0530

    change

---
+++ simplemaven1/src/main/java/simplemaven4/change.java
@@ -0,0 +1,11 @@
+package simplemaven4;
+
+import org.testng.Reporter;
+
+public class change {
+	public void Demo() {
+		Reporter.log("learing syn fork",true);
+		
+	}
+
+}
---
+++ simplemaven1/src/main/java/simplemaven4/package-info.java
@@ -0,0 +1,1 @@
+package simplemaven4;
